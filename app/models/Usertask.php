294a49ad4123b0ff2b01f32d2e9ca2b850d86a3a
lan <lan@landeMacBook-Air.local>
2014-06-19 09:49:00 +0800

    usertask

--- app/models/Usertask.php
+++ app/models/Usertask.php
@@ -3,4 +3,14 @@
 class Usertask extends \Eloquent {
 	protected $table = 'user_task';
 	protected $fillable = array();
+
+	public function users()
+	{
+		return $this->hasMany('User');
+	}
+
+	public function tasks()
+	{
+		return $this->hasMany('Task');
+	}
 }
